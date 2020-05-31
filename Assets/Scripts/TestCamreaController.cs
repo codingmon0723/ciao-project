36da31ee2e9b2a53ef006294a7b403a04b864db7
Coco코코 <rkdcksgud1@gmail.com>
2020-05-31 13:25:53 +0900

    Add sprites -arrow_effect -boom_effect -slash_effect -treasure_box
    Add scripts
    -TestCameraContorller

---
+++ Assets/Scripts/TestCamreaController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCamreaController : MonoBehaviour {
+
+	public float speed = 10f;
+
+	void Update () {
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+
+		Vector3 direction = new Vector3(h, v).normalized;
+
+		transform.Translate(direction * speed * Time.deltaTime);
+	}
+}
